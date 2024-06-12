bd768ced9300c5edb7855e2be187139c60e3e45f
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-06-12 23:48:20 +0200

    Add files via upload

---
+++ StringFormat/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace StringFormat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double kwota = 17.75;
+            Console.WriteLine(kwota.ToString("C"));
+
+            string napis = $"{kwota:c}";
+            Console.WriteLine(napis);
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now.ToString("dd MMMM yyyy"));
+
+            string myString = "wAr aNd pEaCe";
+            TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+        }
+    }
+}
